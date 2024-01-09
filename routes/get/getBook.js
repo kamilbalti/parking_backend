a234e5040ca52e0248984308e2e7b6eb6994fd5f
Mohammad Kamil <mohammad.kamilbalti@gmail.com>
2024-01-09 23:49:12 +0500

    booking condition update

--- routes/get/getBook.js
+++ routes/get/getBook.js
@@ -6,9 +6,8 @@ const router = express.Router();
 router.post("/", validateUser, async(req, res) => {
     Book.findOne({parentId: req?.body?._id}).then((data) => {
         data?.array?.map((item, index) => {
-            item?.bookstarttime = item?.bookstarttime.replace('T', ' ').slice(0, item?.bookstarttime?.length-9)
-            item?.bookstarttime = item?.bookstarttime.replace('T', ' ').slice(0, item?.bookstarttime?.length-9)
-            return item
+            item.bookstarttime = item?.bookstarttime.replace('T', ' ').slice(0, item?.bookstarttime?.length-9)
+            item.booklasttime = item?.booklasttime.replace('T', ' ').slice(0, item?.booklasttime?.length-9)
         })
         return res?.status(200)?.json(data)
     }).catch((err) => console.log('Error', err))
