d4931bcb33522c0740782ef2a59bd35633974e90
Mohammad Kamil <mohammad.kamilbalti@gmail.com>
2024-01-10 18:07:12 +0500

    profile condition update

--- routes/post/Profile.js
+++ routes/post/Profile.js
@@ -6,11 +6,8 @@ const dayjs = require('dayjs');
 router.post("/", async (req, res) => {
     const { name, password, email, newPassword } = req.body
     let tempProfile = {}
-    // let updateName = false;
-    // let updatePassword = false;
     let user = await User.findOne({ email: email })
     const passCheck = await bcrypt.compare(password, user.password)
-    // console.log(req.body)
 
     try {
         if (await name && user.name !== name) {
@@ -20,15 +17,14 @@ router.post("/", async (req, res) => {
         if (await newPassword && passCheck) {
             user.password = await bcrypt.hash(newPassword, 8)
             tempProfile.password = await bcrypt.hash(newPassword, 8)
-            // console.log(user)
         }
         else if (newPassword)
             return res.status(401).json("User Password is not match \nPlease provide Correct Password")
         console.log( !!tempProfile)
-        if ( await !!tempProfile) {
-            user.updatedAt = dayjs()?.format('YYYY-MM-DD')
-            await user.save()
+        if (!!tempProfile) {
+            // user.updatedAt = dayjs()?.format('YYYY-MM-DD')
             console.log(tempProfile)
+            await user.save()
             return res.status(200).json(await user )
         }
         else return res.status(401).json('Profile is not Changed Please fill the fields to update Profile!')
